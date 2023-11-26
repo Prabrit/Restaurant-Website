6fd23b98fd3f63af5232a5f392e3bf7596827de1
Prabrit <127549439+Prabrit@users.noreply.github.com>
2023-11-26 23:04:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const coordinates = [22.719330,88.485690];
+
+const map = L.map('map').setView(coordinates, 16);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker(coordinates).addTo(map);
+
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+
+  });
